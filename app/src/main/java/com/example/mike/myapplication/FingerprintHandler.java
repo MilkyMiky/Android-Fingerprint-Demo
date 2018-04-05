232560ff1dbac71c73da8f0af13e87236ab48dc6
mike <milkymiky395@gmail.com>
2018-04-05 21:14:08 +0300

    Create project

---
+++ app/src/main/java/com/example/mike/myapplication/FingerprintHandler.java
@@ -0,0 +1,59 @@
+package com.example.mike.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+class FingerprintHandler extends FingerprintManager.AuthenticationCallback{
+
+    private Context context;
+
+    public FingerprintHandler(Context context) {
+        this.context = context;
+    }
+
+    public void startAuth(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject) {
+
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(context,"oGRANTED",Toast.LENGTH_LONG).show();
+
+        }
+
+        fingerprintManager.authenticate(cryptoObject,cancellationSignal,
+                0,this,null);
+    }
+
+    @Override
+    public void onAuthenticationError(int errorCode, CharSequence errString) {
+        super.onAuthenticationError(errorCode, errString);
+        Toast.makeText(context,"onAuthenticationError",Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+        super.onAuthenticationHelp(helpCode, helpString);
+        Toast.makeText(context,"onAuthenticationHelp",Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
+        Toast.makeText(context," onAuthenticationSucceeded",Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        super.onAuthenticationFailed();
+        Toast.makeText(context,"onAuthenticationFailed",Toast.LENGTH_LONG).show();
+
+    }
+}
